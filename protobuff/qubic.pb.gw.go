55f1b2902be044118b389e7adfd96a9d74c52faa
0xluk <luk@luktech.dev>
2024-08-01 11:56:54 +0300

    added more balance related fields to GetBalance request

--- protobuff/qubic.pb.gw.go
+++ protobuff/qubic.pb.gw.go
@@ -88,7 +88,11 @@ func request_QubicLiveService_BroadcastTransaction_0(ctx context.Context, marsha
 	var protoReq BroadcastTransactionRequest
 	var metadata runtime.ServerMetadata
 
-	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -101,7 +105,11 @@ func local_request_QubicLiveService_BroadcastTransaction_0(ctx context.Context, 
 	var protoReq BroadcastTransactionRequest
 	var metadata runtime.ServerMetadata
 
-	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
